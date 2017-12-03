dead6dacc214ab55e20dc8a9e33b0c4f23b4f7e5
Your Name <you@example.com>
2017-12-03 22:55:20 +0800

    asdf

---
+++ main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>留言板 By OldDog</title>
+</head>
+<body>
+	<form action="/message.php" method="post">
+		你的留言内容：
+		<textarea name="message"></textarea>
+		<input type="submit" value="提交">
+	</form>
+</body>
+</html>
